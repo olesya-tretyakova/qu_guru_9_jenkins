7c59c60a978319088cade05696add91a88ef2cc3
Olesya Tretyakova <OTretyakova@beeline.kz>
2021-10-02 14:36:14 +0600

    added simple tests

---
+++ src/test/java/test/simple/SkippedTests.java
@@ -0,0 +1,26 @@
+package test.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+    @Test
+    @Disabled
+    void test0() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled("With some reason")
+    void test2() {
+        assertTrue(true);
+    }
+}

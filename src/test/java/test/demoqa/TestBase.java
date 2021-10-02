ce0f7f77164d7b4f573ee14e68ff76a89bf7b062
Olesya Tretyakova <OTretyakova@beeline.kz>
2021-10-02 15:24:10 +0600

    added allure ToolsQATest

---
+++ src/test/java/test/demoqa/TestBase.java
@@ -0,0 +1,16 @@
+package test.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLog;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void setup(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.startMaximized =true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+}
--- src/test/java/test/demoqa/ToolsQATest.java
+++ src/test/java/test/demoqa/ToolsQATest.java
@@ -1,68 +1,94 @@
 package test.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
-public class ToolsQATest {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.startMaximized = true;
-    }
+public class ToolsQATest extends TestBase {
+
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            gender = "Other",
+            mobile = faker.number().digits(10),
+            dayOfBirth = "10",
+            monthOfBirth = "May",
+            yearOfBirth = "1988",
+            subject1 = "Chemistry",
+            subject2 = "Commerce",
+            hobby1 = "Sports",
+            hobby2 = "Reading",
+            hobby3 = "Music",
+            picture = "1.png",
+            currentAddress = faker.address().fullAddress(),
+            state = "Uttar Pradesh",
+            city = "Merrut";
 
     @Test
-    void fillFormTest() {
-        String firstName = "Tom";
-        String lastName = "Cruise";
-        String email = "tom_cruise@gmail.com";
-        String gender = "Male";
-        String mobileNumber = "1234512345";
-        String address = "USA";
-        String state = "NCR";
-        String city = "Noida";
-        String subject = "Math";
-        String day = "3";
-        String month = "July";
-        String year = "1962";
-        String hobby = "Sports";
-        String fileName = "photo.jpg";
+    void successfulFillFormTest() {
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        });
 
-        open("https://demoqa.com/automation-practice-form");
+        step("Fill students registration form", () -> {
+            step("Fill common data", () -> {
+                $("#firstName").val(firstName);
+                $("#lastName").val(lastName);
+                $("#userEmail").val(email);
+                $("#genterWrapper").$(byText(gender)).click();
+                $("#userNumber").val(mobile);
+            });
+            step("Set date", () -> {
+                $("#dateOfBirthInput").clear();
+                $(".react-datepicker__month-select").selectOption(monthOfBirth);
+                $(".react-datepicker__year-select").selectOption(yearOfBirth);
+                $(".react-datepicker__day--0" + dayOfBirth).click();
+            });
+            step("Set subjects", () -> {
+                $("#subjectsInput").val(subject1);
+                $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
+                $("#subjectsInput").val(subject2);
+                $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
+            });
+            step("Set hobbies", () -> {
+                $("#hobbiesWrapper").$(byText(hobby1)).click();
+                $("#hobbiesWrapper").$(byText(hobby2)).click();
+                $("#hobbiesWrapper").$(byText(hobby3)).click();
+            });
+            step("Upload image", () ->
+                    $("#uploadPicture").uploadFromClasspath("img/" + picture));
+            step("Set address", () -> {
+                $("#currentAddress").val(currentAddress);
+                $("#state").scrollTo().click();
+                $("#stateCity-wrapper").$(byText(state)).click();
+                $("#city").click();
+                $("#stateCity-wrapper").$(byText(city)).click();
+            });
+            step("Submit form", () ->
+                    $("#submit").click());
+        });
 
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
-        $("#genterWrapper").$(byText(gender)).click();
-        $("#userNumber").setValue(mobileNumber);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__year-select").selectOption(year);
-        $(byText(day)).click();
-        $("#subjectsInput").setValue(subject).pressEnter();
-        $("#hobbiesWrapper").$(byText(hobby)).click();
-        $("#uploadPicture").uploadFromClasspath(fileName);
-        $("#currentAddress").setValue(address);
-        $("#stateCity-wrapper").$(byText("Select State")).scrollIntoView(true).click();
-        $("#stateCity-wrapper").$(byText(state)).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText(city)).click();
-        $("#submit").click();
-
-        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(firstName + " " + lastName));
-        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text(email));
-        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text(gender));
-        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(mobileNumber));
-        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text(day + " " + month + "," + year));
-        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text(subject));
-        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text(hobby));
-        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text(fileName));
-        $(".table-responsive").$(byText("Address")).parent().shouldHave(text(address));
-        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text(state + " " + city));
+        step("Verify successful form submit", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+            $x("//td[text()='Student Email']").parent().shouldHave(text(email));
+            $x("//td[text()='Gender']").parent().shouldHave(text(gender));
+            $x("//td[text()='Mobile']").parent().shouldHave(text(mobile));
+            $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+            $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
+            $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3));
+            $x("//td[text()='Picture']").parent().shouldHave(text(picture));
+            $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+            $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+        });
     }
 }
